3812f0f63cac2da6a53c48aa771549225724e38a
Mikołaj Cebula <99285995+mikolajcebula@users.noreply.github.com>
2023-10-29 13:56:36 +0100

    Update Wektor2D.hpp

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,68 +1,62 @@
-class Wektor2D
-{
-   private:
+class Wektor2D {
+private:
     double x;
     double y;
-    static int num_wek;
 
 public:
-    Wektor2D() : x(0.0), y(0.0) {
-        ++num_wek;
-    }
+    Wektor2D()
+    {
+        x=0;
+        y=0;
+        // ++num_wek;
 
-    Wektor2D(double val1, double val2, bool polar = false) {
-        ++num_wek;
-        if (polar) {
-            x = val1 * cos(val2);
-            y = val1 * sin(val2);
-        } else {
-            x = val1;
-            y = val2;
-        }
     }
-
+    Wektor2D(double a, double b) : x(a), y(b) 
+    {
+        cout << "Współrzędne wektora = "<< x << ", "<< y << "\n";
+    }
     ~Wektor2D() {
-        --num_wek;
+        cout << "Dekonstruktor, współrzędne wektora = "<< x << ", "<< y <<"\n";
     }
 
-    static int populacja() {
-        return num_wek;
-    }
-
-    double getX() const {
-        return x;
-    }
-
-    double getY() const {
-        return y;
-    }
-
-    double norm() const {
-        return sqrt(x * x + y * y);
-    }
-
-    void print() const {
-        std::cout << "(" << x << ", " << y << ")\n";
-    }
-
-    Wektor2D operator+(const Wektor2D& other) const {
-        return Wektor2D(x + other.x, y + other.y);
-    }
-
-    Wektor2D operator*(double scalar) const {
-        return Wektor2D(x * scalar, y * scalar);
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Wektor2D& wektor) {
-        os << "(" << wektor.x << ", " << wektor.y << ")";
-        return os;
-    }
-
+    // static int populacja() {
+    //     return num_wek;
+    // }
     void setX(double val) {
         x = val;
     }
 
     void setY(double val) {
         y = val;
+    }
+    double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
+
+    double norm() {
+        return sqrt(x * x + y * y);
+    }
+
+    void print() {
+        cout << "Współrzędne wektora = " << x << ", " << y << ")\n";
+    }
+    Wektor2D operator+(Wektor2D &w1)
+    {   
+        Wektor2D wynik;
+        wynik.x = x + w1.x;
+        wynik.y = y + w1.y;
+        return wynik;
+    }
+    double operator*(Wektor2D &w1)
+    {
+        Wektor2D wynik;
+        wynik.x = x * w1.x;
+        wynik.y = y * w1.y;
+        double wyn =  wynik.getX() + wynik.getX();
+        return wyn;
     }
 };
